45b328a0ce946e473dd72d4ae51cac7b1b2822e0
eyasuyid <eyasuyid@gmail.com>
2022-03-11 17:50:45 +0300

    this is project 0x01

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,7 +16,7 @@ int main(void)
     else if(n>0){
         printf("%d is positive", n);
     } else {
-        printf("%d is negative");
+        printf("%d is negative", n);
     }
 	return (0);
 }
