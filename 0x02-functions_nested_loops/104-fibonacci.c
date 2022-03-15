3c5a7bd786dae5d602ce484fb78c64a891c1b75f
eyasuyid <eyasuyid@gmail.com>
2022-03-15 22:34:31 +0300

    function and nested loop

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,7 @@ int main(void)
 	unsigned long int num1 = 1, num2 = 2, count = 0, sum;
 
 	printf("%li, %li", num1, num2);
-	while (count < 96)
+	while (count < 94)
 	{
 		sum = num1 + num2;
 		printf(", %lu", sum);
