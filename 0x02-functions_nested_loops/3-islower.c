1f420be95a7df8c2a8fbe11af1d4d33c66dc99a7
eyasuyid <eyasuyid@gmail.com>
2022-03-15 17:09:23 +0300

    function and nested loop

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * _islower - check upppercase and lowercase
  *
- * Return: Always 0.
+ * @c: char parameter from the user
+ * Return: 0 if uppercase, 1 if lowercase.
  */
-int main(void)
-{
-    int r;
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+
+int _islower(int c)
+{
+	if (c >= 'a')
+	{
+		return (1);
+	}
+	return (0);
 }
