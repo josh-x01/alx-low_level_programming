3fec2f1585d7d18d74429642e07e3158000c65d1
eyasuyid <eyasuyid@gmail.com>
2022-03-15 19:17:22 +0300

    function and nested loop

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,9 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
-		n = -n;
-		_putchar(n % 10 + '0');
-		return (n % 10);
+		unsigned m = n;
+		_putchar(m % 10 + '0');
+		return (m % 10);
 	}
 	_putchar(n % 10 + '0');
 	return (n % 10);
