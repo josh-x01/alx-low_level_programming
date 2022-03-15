66b33bcec31da92d195c56a1a4af56810c4b58ae
eyasuyid <eyasuyid@gmail.com>
2022-03-15 21:47:30 +0300

    function and nested loop

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	long int num1 = 1, num2 = 2, count = 0, sum;
 
-	printf("%li, %li,", num1, num2);
+	printf("%li, %li", num1, num2);
 	while (count < 48)
 	{
 		sum = num1 + num2;
 
-		printf(" %li,", sum);
+		printf(" ,%li", sum);
 		num1 = num2;
 		num2 = sum;
 		count++;
