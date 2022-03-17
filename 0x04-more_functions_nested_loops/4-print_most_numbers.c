99549d092879e62d3504d50f10a9428c8464e552
eyasuyid <eyasuyid@gmail.com>
2022-03-17 14:28:03 +0300

    this is project 0x03

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - print 0 - 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
