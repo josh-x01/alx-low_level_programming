36b4508938ef9281981966cee09e4b8e25643574
eyasuyid <eyasuyid@gmail.com>
2022-03-17 15:38:09 +0300

    this is project 0x03

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * print_line - prints "_" n time
+ * print_line - print "_" n times
  * @n: number of "_"
  */
 void print_line(int n)
 {
-	while (n--)
+	while (n > 0)
 	{
 		_putchar('_');
+		n--;
 	}
 	_putchar('\n');
 }
