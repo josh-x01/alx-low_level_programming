953c3274cb8f86b19b34114b00c724dc4ff33fe1
eyasuyid <eyasuyid@gmail.com>
2022-03-17 15:53:27 +0300

    this is project 0x03

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_diagonal - print a diagonal line
+ * @n: length of diagonal
+ */
+void print_diagonal(int n)
+{
+	int space = 0, i;
+
+	while (n > 0)
+	{
+		for (i = 0; i < space; ++i)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		n--;
+		space++;
+		_putchar('\n');
+	}
+}
