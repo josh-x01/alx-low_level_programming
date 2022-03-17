40badc5e228b8f7504dc743dc3f62844d2d03dbb
eyasuyid <eyasuyid@gmail.com>
2022-03-17 21:02:05 +0300

    this is project 0x03

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,12 +7,19 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; ++i)
+	if (size > 0)
 	{
-		for (j = 0; j < size; ++j)
+		for (i = 0; i < size; ++i)
 		{
-			_putchar('#');
+			for (j = 0; j < size; ++j)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
