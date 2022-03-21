3556c27fe52f91ee8d301d0abbf1000470867683
eyasuyid <eyasuyid@gmail.com>
2022-03-21 22:23:18 +0300

    array, pointer and string

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,12 +10,17 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < (n - 1); i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[i]);
+		int num = *(a + i);
+
 		if (i == (n - 1))
 		{
 			printf("%d", a[n - 1]);
+		}
+		else
+		{
+			printf("%d, ", num);
 		}
 	}
 	printf("\n");
