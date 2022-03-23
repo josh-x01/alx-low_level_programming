79a9327a7b0b95a84926ae2c2dd6b71137273d8f
eyasuyid <eyasuyid@gmail.com>
2022-03-23 23:30:14 +0300

    More on pointers array and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,38 +1,40 @@
 #include "main.h"
-
 /**
- * *_strncat - concatenates two strings.
- *
- * @dest: resulting string.
- * @src: source string.
- * @n: string length.
- * Return: pointer to dest
+ * size - find the size of the array
+ * @str: the array
+ * Return: the size of array
+ */
+int size(char *str)
+{
+	char str_size = str[0];
+	int n = 1;
+
+	while (str_size != '\0')
+	{
+		str_size = str[n];
+		n++;
+	}
+	return (n - 1);
+}
+/**
+ * _strcat - concatinate src on dest
+ * @dest: the first array
+ * @src: the second array
+ * Return: dest + src
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, m;
+	int ns = size(dest), i;
 
-	m = string_length(dest);
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != src[n]; i++)
 	{
-		dest[m + i] = src[i];
+		if (src[i] == '\0')
+		{
+			break;
+		}
+		dest[ns + i] = src[i];
 	}
-	dest[m + i] = '\0';
+	dest[ns + i] = src[n];
+
 	return (dest);
-}
-
-/**
- * string_length - finds the length of a string.
- * Return: length of c.
- * @pointer: pointer.
- */
-int string_length(char *pointer)
-{
-	int c = 0;
-
-	while (*(pointer + c) != '\0')
-	{
-		c++;
-	}
-	return (c);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@ int _putchar(char);
 char *_strcat(char *dest, char *src);
 int string_length(char *pointer);
 char *_strncat(char *dest, char *src, int n);
+int size(char *str);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
