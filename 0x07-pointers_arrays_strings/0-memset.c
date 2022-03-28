8879d66357f36b85bd1d222d846d9b48e4e90bab
eyasuyid <eyasuyid@gmail.com>
2022-03-28 22:28:34 +0300

    more on pointer, array and string

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,16 @@
 #include "main.h"
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: is the direction you are going
+ * @b: constant character variable
+ * @n: is the limit to which you will reach
+ * Return: s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
 
-char *_memset(char *s, char b, unsigned int n){
-    memset(s, b, n);
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+	return (s);
 }
