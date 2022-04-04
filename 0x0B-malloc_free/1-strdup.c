edaef845602115bb1e59d626b190431e8cd185d7
eyasuyid <eyasuyid@gmail.com>
2022-04-05 00:11:37 +0300

    memory allocation

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * countStr - counts the number of char
+ * @str: the string to be counted
+ * Return: the size of string
+ */
+int countStr(char *str)
+{
+	int size = 0;
+
+	while (*str++ != '\0')
+	{
+		size++;
+	}
+	return (size);
+}
+/**
+ * _strdup - deplicate the str
+ * @str: str to be duplicated
+ * Return: the new string
+ */
+char *_strdup(char *str)
+{
+	int size = countStr(str);
+	char *word = malloc(size);
+
+	while (size--)
+	{
+		word[size] = str[size];
+	}
+	return (word);
+}
