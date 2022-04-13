77d378b8ebef325acb06d8bafb0bf49aab9146b8
eyasuyid <eyasuyid@gmail.com>
2022-04-13 21:48:38 +0300

    pointer to function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
-#include "main.h"
+#include "function_pointers.h"
+
 /**
- * print_name - it print name
- * @name: the name to be printed
- * @f: pointer of function
+ * print_name - prints a name
+ * @name: name's main
+ * @f: void function
  */
-void print_name(char *name, void(*f)(char *))
+
+void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
 	{
 		f(name);
 	}
 }
-
