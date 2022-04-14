5b46426ab98931c6748a3c47d6ce71a648cbfe40
eyasuyid <eyasuyid@gmail.com>
2022-04-14 14:18:15 +0300

    variadic

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,3 +1,10 @@
+#ifdef _VARIADIC_FUNCTIONS_H
+#define _VARIADIC_FUNCTION_H
+#include <stdarg.h>
+#include <stdio.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+#endif
