961da390ee5c1834cd07489aa61da8a7452c1548
root <root@Joshua.localdomain>
2022-04-21 20:38:24 +0300

    single linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,14 @@
 #include "lists.h"
 #include <stdio.h>
-
+/**
+ * print_list - print linked list data
+ * @h: the head of the node
+ * Retrun: return the number of node
+ */
 size_t print_list(const list_t *h)
 {
 	int numberOfNode = 0;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
