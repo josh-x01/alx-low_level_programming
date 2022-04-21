aaf26a0025a09257c05efba57ec30a68713de352
root <root@Joshua.localdomain>
2022-04-21 20:54:23 +0300

    single linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,13 +4,13 @@
  * list_len - it cal the size of the linked list
  * @h: the head of the linked list
  *
- * Return the size of linked list
+ * Return: the size of linked list
  */
 
 size_t list_len(const list_t *h)
 {
 	int n = 0;
-	
+
 	while (h)
 	{
 		n++;
