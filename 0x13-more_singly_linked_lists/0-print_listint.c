5fd432094f98c627495c6dda82de04bfbc31ab95
eyasuyid <eyasuyid@gmail.com>
2022-04-26 14:11:26 +0300

    all project

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - print all integer in the linked list
+ * @h: the head of linked list
+ * Return: the size of the linked list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *temp = h;
+	int size = 0;
+
+	while (temp != NULL)
+	{
+		printf("%i\n", temp->n);
+		temp = temp->next;
+		size++;
+	}
+
+	return (size);
+}
