3c1b5bbf063ad236baf66970173740a14cad7eae
eyasuyid <eyasuyid@gmail.com>
2022-04-26 17:28:44 +0300

    all projects

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,18 +1,17 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_listint2 - free head of the linked list
- * @head: the first node
- */
-
+ * free_listint2 - Frees a list.
+ * @head: Address of the first node of a list.
+ **/
 
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
-
 	while (*head != NULL)
 	{
 		temp = (*head)->next;
