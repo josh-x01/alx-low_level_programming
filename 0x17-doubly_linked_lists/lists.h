0d9ecbd75d376886ca8f6c0a994b14c6dbb6684a
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-10 00:44:59 +0300

    Double linked list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,3 @@
-#ifndef _list_
-#define _list_
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -10,9 +8,8 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ * for Holberton project
  */
-
 typedef struct dlistint_s
 {
     int n;
@@ -20,6 +17,7 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
+/*Prototypes interface*/
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
