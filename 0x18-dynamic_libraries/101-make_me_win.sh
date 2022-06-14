1bc63dc6eb962b483e76f828cf1a8801b382c497
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-14 18:40:49 +0300

    dynamic library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget /tmp https://raw.githubusercontent.com/eyasuyid/alx-low_level_programming/main/0x18-dynamic_libraries/libmask.so
+wget -P /tmp https://raw.githubusercontent.com/eyasuyid/alx-low_level_programming/main/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
