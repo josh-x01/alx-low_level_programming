947936b00537f0805c4ced2b5d45e8850160a0b1
eyasuyid <eyasuyid@gmail.com>
2022-10-05 23:10:00 +0300

    GOD

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,21 +1,6 @@
 #include "search_algos.h"
 
 /**
- * min - finds the min for the arg.
- * @a: first number.
- * @b: second number.
- * Return: the smaller number.
-*/
-
-int min(int a, int b)
-{
-	if (a < b)
-		return (a);
-	return (b);
-}
-
-
-/**
  * jump_search - search value in the given array
  * @array: array contain numbers
  * @size: the size of the array
@@ -26,32 +11,44 @@ int min(int a, int b)
 
 int jump_search(int *array, size_t size, int value)
 {
-	int s = size;
-	int l = 0, j = sqrt(size);
-	int h = j;
+	size_t start, end, step;
 
-	while (size--)
+	if (!array || size == 0)
+		return (-1);
+	step = sqrt(size);
+	start = 0, end = step;
+
+	while (start < size)
 	{
-		printf("Value checked array[%i] = [%i]\n", l, array[l]);
-		if (l >= s)
-			break;
-		if (array[l] <= value && array[h] >= value)
-			break;
-		if (h >= s)
-			break;
-		l = h;
-		h += j;
-	}
-	printf("Value found between indexes [%i] and [%i]\n", l, h);
-	while (array[l] <= value)
-	{
-		printf("Value checked array[%i] = [%i]\n", l, array[l]);
-		if (array[l] == value)
-			break;
-		l++;
+		printf("Value checked array[%lu] = [%d]\n", start, array[start]);
+		if (end < size)
+		{
+			if (array[start] <= value && value <= array[end])
+			{
+				printf("Value found between indexes [%lu] and [%lu]\n", start, end);
+				break;
+			}
+		} else
+		{
+			if (array[start] <= value)
+			{
+				printf("Value found between indexes [%lu] and [%lu]\n", start, end);
+				break;
+			}
+		}
+		start = end;
+		end = start + step;
 	}
 
-	if (array[l] == value)
-		return (l);
+	while (start <= end)
+	{
+		if (start == size)
+			return (-1);
+		printf("Value checked array[%lu] = [%d]\n", start, array[start]);
+		if (array[start] == value)
+			return (start);
+		start++;
+	}
+
 	return (-1);
 }
